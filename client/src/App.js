45e5f60bd0d9682e8cdc4742a4f073c4e6f14146
Kyle <kylevance801@gmail.com>
2022-11-17 19:21:36 -0700

    scrapped npm react vote packge, using another. Have template files with sweater templates made.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,44 @@
 import './css/App.css';
-
-// importing different sections
-import Container from './components/container';
-
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import VotingCard from "./components/VotingCard";
+import sweaterJson from "./lib/sweaters.json";
+import "./assets/scss/styles.scss";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
+  let [sweater, setsweater] = useState([]);
+
+  useEffect(() => {
+    setsweater(sweaterJson);
+  }, []);
+
+  function incrementVoteCount(teamId) {
+    sweater = sweater.map((team) => {
+      if (team._id === teamId) {
+        team.votes = team.votes + 1;
+      }
+      return team;
+    });
+    setsweater(sweater);
+  }
+
   return (
-    <Container />
+    <Container className="app">
+      <Row>
+        {sweater.map((team) => {
+          return (
+            <Col md={4}>
+              <VotingCard
+                team={team}
+                incrementVoteCount={(teamId) => incrementVoteCount(teamId)}
+              />
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
   );
 }
-
 export default App;
+
---
+++ client/src/components/votingCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+
+function VotingCard(props) {
+  let { sweater, incrementVoteCount } = props;
+
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={`/assets/images/${sweater.pic}`} />
+      <Card.Body>
+        <Card.Title>{sweater.name}</Card.Title>
+        <Button variant="success" onClick={() => incrementVoteCount(sweater._id)}>
+          Vote
+        </Button>
+      </Card.Body>
+      <Card.Footer>Vote count: {sweater.votes}</Card.Footer>
+    </Card>
+  );
+}
+export default VotingCard;
