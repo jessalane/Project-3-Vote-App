cf9874070120e681c2bf9cbe71a79013da77264e
Kyle <kylevance801@gmail.com>
2022-11-17 19:53:22 -0700

    poll functions working with proof

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,19 @@
+
+
+
+
+
+// -------
 import './css/App.css';
-import React, { useEffect, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import VotingCard from "./components/VotingCard";
-import sweaterJson from "./lib/sweaters.json";
-import "./assets/scss/styles.scss";
-import "bootstrap/dist/css/bootstrap.css";
+
+// importing different sections
+import Container from './components/container';
+
 
 function App() {
-  let [sweater, setSweater] = useState([]);
-
-  useEffect(() => {
-    setSweater(sweaterJson);
-  }, []);
-
-  function incrementVoteCount(sweaterId) {
-    sweater = sweater.map((sweater) => {
-      if (sweater._id === sweaterId) {
-        sweater.votes = sweater.votes + 1;
-      }
-      return sweater;
-    });
-    setSweater(sweater);
-  }
-
   return (
-    <Container className="app">
-      <Row>
-        {sweater.map((sweater) => {
-          return (
-            <Col md={4}>
-              <VotingCard
-                sweater={sweater}
-                incrementVoteCount={(sweaterId) => incrementVoteCount(sweaterId)}
-              />
-            </Col>
-          );
-        })}
-      </Row>
-    </Container>
+    <Container />
   );
 }
-export default App;
 
+export default App;
