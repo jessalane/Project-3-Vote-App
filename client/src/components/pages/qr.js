bf2a5599dd30b67899fa55698ff712e6f4563b0f
Kyle <kylevance801@gmail.com>
2022-11-17 22:31:38 -0700

    added some comments for qr.js for future direction

--- client/src/components/pages/qr.js
+++ client/src/components/pages/qr.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import QRCode from "react-qr-code";
 
 ReactDOM.render(<QRCode value="insert link here" />, document.getElementById("Container"),
-
+// template for styling and displaying the QR code, will need to be moved.
 <div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}>
     <QRCode
     size={256}
     style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-    value={value}
+    // value={value}
     viewBox={`0 0 256 256`}
     />
 </div>
