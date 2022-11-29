147ef0348c3161ce1220aebaebb14425b3b17a93
Kyle <kylevance801@gmail.com>
2022-11-28 21:50:30 -0700

    formatting of the newpoll page

--- client/src/components/pages/newPoll.js
+++ client/src/components/pages/newPoll.js
@@ -2,7 +2,6 @@ import '../../css/newPoll.css';
 import { ADD_POLL } from '../../utils/mutations';
 import { useMutation } from '@apollo/client';
 import React, {
-  useRef,
   useState,
   useEffect
 } from 'react';
@@ -13,50 +12,49 @@ import Uploader from './uploader';
 
 function NewPoll() {
   // uploader 
-  const  setPage = useState('Home');
   const [file, setFile] = useState(null);
   const [inputId, setInputId] = useState(0);
   // TODO: set state to true once submitted
   // const [submitted, setSubmitted] = useState(false);
   useEffect(() => {
-    if (file!==null) {
+    if (file !== null) {
       const newValue = {};
       newValue.name = pollState.options[inputId].name;
       newValue.image = file?.filesUploaded[0]?.url
-    
-    const oldOptions = [...pollState.options];
-    oldOptions.splice(parseInt(inputId), 1, newValue);
-    
 
-    setPollState({
-      ...pollState,
-      options: [...oldOptions]
-    })};
+      const oldOptions = [...pollState.options];
+      oldOptions.splice(parseInt(inputId), 1, newValue);
+
+
+      setPollState({
+        ...pollState,
+        options: [...oldOptions]
+      })
+    };
   }, [file]);
 
-    const inputEl = useRef(null);
-    const onButtonClick = (event) => {
-        event.preventDefault();
-        const { id } = event.target.dataset;
-        setInputId(id)
-        
-    Uploader({ setFile, file})
-    };
+  const onButtonClick = (event) => {
+    event.preventDefault();
+    const { id } = event.target.dataset;
+    setInputId(id)
+
+    Uploader({ setFile, file })
+  };
 
   let user = localStorage.getItem("user_email");
 
   const [pollState, setPollState] = useState({ title: '', options: [{ name: '', image: '' }, { name: '', image: '' }, { name: '', image: '' }, { name: '', image: '' }, { name: '', image: '' }, { name: '', image: '' }], author: user });
 
-  const [addPoll, { error}] = useMutation(ADD_POLL);
+  const [addPoll, { error }] = useMutation(ADD_POLL);
 
   const handleChange = (event) => {
     const { id } = event.target.dataset;
     const { name, value } = event.target;
     const newValue = {};
-    if(name === "name") {
+    if (name === "name") {
       newValue.name = value;
       newValue.image = pollState.options[id].image;
-    } else if(name === "title") {
+    } else if (name === "title") {
       // title change
     }
     const oldOptions = [...pollState.options];
@@ -90,7 +88,7 @@ function NewPoll() {
       }]
     });
   };
-  
+
   return (
     <section id="newPoll">
       {/* <QRCode value= {url} /> */}
@@ -107,48 +105,62 @@ function NewPoll() {
           type="text"
           placeholder="What we are voting on"
           className="pollTitle"
-          required
-        />
-        </form>
-        <form
-          onSubmit={handleFormSubmit}
-          className="optionForm"
-        >
-          {pollState.options.map((option, i) => (
-            <div key={i}
-            className="pollGroup">
-              <h2>Option {i+1} </h2>
-            <input
-            value={option.name}
-            onChange={handleChange}
-            data-id={i}
-            name="name"
-            type="text"
-            placeholder={"option " + (i+1)}
-            required
-          />
-          {option.image==="" ? <input
-          value={option.image}
-          // onChange={handleChange}
-          onClick={onButtonClick}
-          data-id={i}
-          name="image"
-          type="button"
-          placeholder={"image " + (i+1)}
-          required
-    
-          />:<img src={ option.image }/>}
           
-        </div>
-          ))}
-          {error && (
+        />
+      </form>
+      <form
+        onSubmit={handleFormSubmit}
+        className="optionForm"
+      >
+        {pollState.options.map((option, i) => (
+          <div key={i}
+            className="pollGroup">
+            <h2>Option {i + 1} </h2>
+            <input
+              value={option.name}
+              onChange={handleChange}
+              data-id={i}
+              name="name"
+              type="text"
+              placeholder={"option " + (i + 1)}
+            
+            />
+            {option.image === "" ? <input 
+              value= "Upload a Photo"
+              onClick={onButtonClick}
+              data-id={i}
+              name="image"
+              type="button"
+              title="Upload a photo"
+              placeholder={"image " + (i + 1)}
+            
+
+            /> : <img src={option.image} alt="a beatiful person" width="300" height="400"/>}
+
+          </div>
+        ))}
+        {error && (
           <div className="newPollErr">
             Something went wrong...
           </div>
         )}
+        {/* submit button */}
+        <input
+          class='submitBtn'
+          style={{ cursor: 'pointer' }}
+          name="Submit"
+          type="submit"
+          text="Submit"
+        />
+        {error && (
+          <div className="newPollErr">
+            Something went wrong...
+          </div>)}
       </form>
 
     </section>
+
+
   );
 };
 export default NewPoll;
